ec1788757e3b2d7552bbdec87b1cf729d6a848c1
Ben <ben@Bens-MacBook-Air.local>
2023-10-12 20:03:58 +0100

    all except notfications

---
+++ hooks/distance.js
@@ -0,0 +1,20 @@
+export const haversineDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = degreesToRadians(lat2 - lat1);
+    const dLon = degreesToRadians(lon2 - lon1);
+
+    const a = 
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(degreesToRadians(lat1)) * Math.cos(degreesToRadians(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = R * c; // Distance in kilometers
+
+    return distance;
+}
+
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
--- hooks/locationTask.js
+++ hooks/locationTask.js
@@ -2,24 +2,131 @@ import * as TaskManager from "expo-task-manager";
 import * as BackgroundFetch from "expo-background-fetch";
 import * as Location from "expo-location";
 import { auth, db } from "../firebaseConfig";
+import {
+	updateDoc,
+	doc,
+	collection,
+	getDocs,
+	getDoc,
+	query,
+	where,
+} from "firebase/firestore";
+import { haversineDistance } from "./distance";
 
 const LOCATION_TRACKING = "location-tracking";
 const BACKGROUND_FETCH_TASK = "background-fetch-task";
 
-const updateLocationData = async (latestLocation) => {
-	const user = auth.currentUser?.uid;
-	if (user) {
-		console.log(locations);
+const checkDistance = async (user, matchedUser) => {
+	try {
+		const userDoc = await getDoc(doc(db, "users", user));
+		if (userDoc.exists()) {
+			const currentUser = userDoc.data();
 
-		const userRef = db.collection("users").doc(user);
-		await userRef.update({
+			const currentUserLocation = currentUser?.lastLocationData;
+			const lat1 = currentUserLocation.latitude;
+			const lon1 = currentUserLocation.longitude;
+
+			const matchedUserLocations = matchedUser?.lastLocationData;
+			const lat2 = matchedUserLocations?.latitude;
+			const lon2 = matchedUserLocations?.longitude;
+
+			const distance = haversineDistance(lat1, lon1, lat2, lon2);
+
+			const notificationSettings =
+				currentUser?.notificationSettings?.rules?.home;
+
+			if (distance <= notificationSettings) {
+				const userDistance = {
+					user: matchedUser,
+					distance: distance,
+				};
+				console.log("checkDistance()", userDistance);
+				return userDistance;
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	} catch (e) {
+		console.log(e);
+		return null;
+	}
+};
+
+const findUserMatches = async (user) => {
+	let userMatches = [];
+
+	try {
+		const contactsRef = collection(doc(db, "users", user), "contacts");
+		const contactsSnapshot = await getDocs(contactsRef);
+		const contactsDocs = contactsSnapshot.docs;
+
+		// 1. Gather all phone numbers
+		let allPhoneNumbers = [];
+		for (let contact of contactsDocs) {
+			let phoneNumbers = contact.data()?.phoneNumbers;
+			for (let phoneNumber of phoneNumbers) {
+				allPhoneNumbers.push(phoneNumber.digits);
+			}
+		}
+		console.log(allPhoneNumbers);
+
+		// 2. Batched queries
+		for (let i = 0; i < allPhoneNumbers.length; i += 10) {
+			const batch = allPhoneNumbers.slice(i, i + 10);
+			const usersRef = collection(db, "users");
+
+			try {
+				const userQuery = query(usersRef, where("phoneNumber", "in", batch));
+				const snapshot = await getDocs(userQuery);
+				// 3. Iterate through results
+
+				snapshot.docs.forEach((doc) => {
+					const docData = doc.data();
+					console.log(docData);
+
+					const matchedUser = { id: doc.id, ...docData };
+					userMatches.push(matchedUser);
+				});
+			} catch (e) {
+				console.log("error with findin phone nmber iin batch", e);
+			}
+		}
+	} catch (error) {
+		console.error("Error fetching user:", error);
+	}
+
+	console.log("findUserMatches()", userMatches);
+	return userMatches;
+};
+
+const updateLocationData = async (latestLocation) => {
+	// const user = auth.currentUser?.uid;
+	// for testing
+	const user = "zqCYH3a8dQPKItEvASScmQEirQ13";
+	console.log(latestLocation);
+
+	try {
+		const userRef = doc(db, "users", user);
+		await updateDoc(userRef, {
 			lastLocationData: {
 				latitude: latestLocation.coords.latitude,
 				longitude: latestLocation.coords.longitude,
 			},
 		});
-	} else {
-		console.log("no user found when trying to updateLocationData()");
+		console.log("User location data uploaded successfully.");
+		const matches = await findUserMatches(user);
+
+		const distances = await Promise.all(
+			matches.map((matchedUser) => checkDistance(user, matchedUser))
+		);
+		console.log(distances);
+		const notificationsToSend = distances.filter(Boolean);
+
+		console.log("notifications to send: ", notificationsToSend);
+	} catch (error) {
+		console.error("Error updating user data:", error);
 	}
 };
 
@@ -28,7 +135,6 @@ export const startLocationTracking = async () => {
 		accuracy: Location.Accuracy.Balanced,
 		timeInterval: 360000,
 		distanceInterval: 0,
-		showsBackgroundLocationIndicator: true,
 	});
 
 	await registerBackgroundFetch();
